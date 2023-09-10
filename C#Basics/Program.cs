c3da9a5ec02ab154e959d3dbf0c7450d2ca0b25e
Abdulrehman Azam <abdulrehmana28@skiff.com>
2023-09-10 11:51:22 +0500

    added more info from last commit

--- C#Basics/Program.cs
+++ C#Basics/Program.cs
@@ -14,6 +14,7 @@ long longValue = 435345;
 bool boolValue = true;
 char charValue = 'G';
 string stringValue = "say hi";
+decimal decimalValue = 34543543.455647457m;
 
 //? There are more date types in C# visit : https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/builtin-types/integral-numeric-types
 
@@ -24,7 +25,7 @@ string stringValue = "say hi";
 // ' """ string """ ' for raw string
 
 Console.WriteLine(@$"This is an integer: {integer}. This is double: {floatValue}. This is float: {floatValue2}.
-This is long: {longValue}. This is bool: {boolValue}. This is char: {charValue}. This is string: {stringValue}");
+This is long: {longValue}. This is bool: {boolValue}. This is char: {charValue}. This is string: {stringValue}. This is decimal: {decimalValue}");
 
 // if else and for loop and while loop is same as java
 //! C# not have do while loop
