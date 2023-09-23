86fbbb2e1121cb681c04ff41852d7cdb7f83a6d8
Abdulrehman Azam <abdulrehmana28@skiff.com>
2023-09-23 14:21:44 +0500

    Completed student grade assignment module 2/6

--- C#Basics3/Program.cs
+++ C#Basics3/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//! Array in C#
+// sane as java 
+int[] arr = {10, 12, 13, 6};
+
+for(int i = 0; i < arr.Length; i++){
+    Console.WriteLine(i);
+}
+
+// ----------------------------------------------------------------
+ Console.WriteLine("================================");
+
+
+ int[] sophiaScores = new int[] { 90, 86, 87, 98, 100 };
+
+
+string[] names = new string[3];
+
+names[0] = "ali ";
+names[1] = "ben ";
+names[2] = "stones ";
+
+for(int i = 0; i < names.Length; i++){
+    Console.WriteLine(names[i]);
+}
+//! for each loop in c#
+foreach (string name in names)
+{
+    Console.WriteLine(name);
+}
