2a10d0a3d21c6b46081bb97c6af8e1950510dd91
Abdulrehman Azam <abdulrehmana28@skiff.com>
2023-09-24 12:05:43 +0500

    completed tenery operator exe 3/2

---
+++ C#Basics5/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("a" == "a ");
+Console.WriteLine("a" == "A");
+Console.WriteLine(1 == 2);
+
+string myValue = "a";
+Console.WriteLine(myValue == "a");
+
+//! Tenery operator in c#
+int saleAmount = 1000;
+int discount = saleAmount > 1000 ? 100 : 50;
+Console.WriteLine($"Discount: {discount}");
+
+Random coin = new Random();
+int flip = coin.Next(0, 2);
+Console.WriteLine((flip == 0) ? "heads" : "tails");
